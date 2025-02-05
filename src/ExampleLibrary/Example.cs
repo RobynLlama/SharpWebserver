fb1ca4cef4fbf19226bbae2c29efa325743762b7
Robyn <rnixon98@gmail.com>
2025-02-05 01:05:36 -0600

    feat: example library

---
+++ src/ExampleLibrary/Example.cs
@@ -0,0 +1,15 @@
+﻿/*
+This library is just a basic example of
+something your webpage may need to call.
+
+Place it in the 'references' folder and
+it will become available to both the 
+runner and compiler automatically
+*/
+
+namespace ExampleLibrary;
+public class MyCoolThing(int number)
+{
+  private readonly int BestNumber = number;
+  public string AmazingMethod => $"Hello World! [{BestNumber}]";
+}
