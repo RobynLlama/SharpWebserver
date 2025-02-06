f09d5a0fa3d8497bd2139075d595e88338f65268
Robyn <rnixon98@gmail.com>
2025-02-05 21:45:45 -0600

    feat: add prefixes to sharpconfig

--- src/SharpWebserver/Config/SharpConfig.cs
+++ src/SharpWebserver/Config/SharpConfig.cs
@@ -1,7 +1,12 @@
+using System.Collections.Generic;
+
 namespace SharpWebserver.Config;
 
 public class SharpConfig
 {
+  public List<string> Prefixes { get; set; } = [
+    "http://*",
+  ];
   public bool SafeMode { get; set; } = false;
   public int PortNumber { get; set; } = 80;
 }
